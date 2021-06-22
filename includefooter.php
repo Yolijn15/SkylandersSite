21042596f5152ad557ecafd3ccf86e6c37d09695
FemkeDerks <75674387+FemkeDerks@users.noreply.github.com>
2021-06-22 09:10:49 +0200

    FOOTER + CSS IN BESCHRIJVING
    .inner-footer{
      
      background-color: purple;
      height: 50px;
    }
    
    .social-links{
      display: flex;
    }
    
    .social-links ul{
      display: flex;
      width: 300px;
      height: 30px;
      margin: auto;
      padding-bottom: 10px;
    }
    
    .social-items{
      float: left;
    }
    
    .social-items a{
      padding: 10px 20px;
      font-size: 35px;
      color: #6cccc6;
      transition: all .25s;
    }
    
    .quick-links{
      display: flex;
      width: 500px;
      height: 70px;
      margin: auto;
    }
    
    .quick-links ul{
      display: flex;
    }
    
    .quick-items{
      list-style: none;
    }
    
    .quick-items a{
      text-decoration: none;
      padding: 0px 10px;
      font-size: 15px;
      color:lightblue;
      text-align: center;
    }
    
    .quick-items a:hover{
      color: darkblue;
    }
    
    .outer-footer{
      padding: 15px;
      text-align: center;
      color:lightblue;
      font-size: 12px;
      background-color: darkmagenta;
    
    }

---
+++ includefooter.php
@@ -0,0 +1,26 @@
+<footer class="footer">
+    <div class="inner-footer">
+      <div class="social-links">
+        <ul>
+          <li class="social-items"><a href="#"></a></li>
+          <li class="social-items"><a href="#"></a></li>
+          <li class="social-items"><a href="#"></a></li>
+          <li class="social-items"><a href="#"></a></li>
+        </ul>
+      </div>
+      <div class="quick-links">
+        <ul>
+          <li class="quick-items"><a href="http://localhost/skylanders/index.php">Home</a></li>
+          <li class="quick-items"><a href="http://localhost/skylanders/about.php">About</a></li>
+          <li class="quick-items"><a href="http://localhost/skylanders/games.php">Games</a></li>
+          <li class="quick-items"><a href="http://localhost/skylanders/games.php">Collection</a></li>
+          <li class="quick-items"><a href="http://localhost/skylanders/contact.php">Contact</a></li>
+        </ul>
+      </div>
+    </div>
+
+    <div class="outer-footer">
+      Copyright &copy; Attack On MBO. All Rights Reserved
+    </div>
+
+  </footer>
